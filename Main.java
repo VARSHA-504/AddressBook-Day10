33f5749083b8956e7aab755ad6fc92532560d96c
varsha-504 <antargangivarsha.com>
2022-03-25 00:24:07 +0530

    program to create new contact

--- Main.java
+++ Main.java
@@ -1,8 +1,43 @@
 package com.bl.addressbook;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Welcome to Address book program");
+        static Scanner sc = new Scanner(System.in);
+
+        public static void main(String[] args) {
+
+            System.out.println("Welcome to Address Book Program");
+
+            AddressBook book = new AddressBook();
+            Contact person1 = new Contact();
+
+            System.out.println("Enter First Name: ");
+            person1.setFirstName(sc.nextLine());
+            System.out.println("Enter Last Name: ");
+            person1.setLastName(sc.nextLine());
+            System.out.println("Enter Address: ");
+            person1.setAddress(sc.nextLine());
+            System.out.println("Enter City: ");
+            person1.setCity(sc.nextLine());
+            System.out.println("Enter State: ");
+            person1.setState(sc.nextLine());
+            System.out.println("Enter Zip Code: ");
+            person1.setZip(sc.nextLine());
+            System.out.println("Enter Phone Number: ");
+            person1.setPhoneNumber(sc.nextLine());
+            System.out.println("Enter E-Mail: ");
+            person1.seteMail(sc.nextLine());
+            System.out.println("Add More Entry?");
+
+            book.setContact(person1);
+            System.out.println("""
+                =============================
+                
+                """);
+            book.getContact();
+        }
     }
 }
